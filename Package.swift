b400f462b4f2f93b129e7e61eedbc3c3cb1e846e
MaestroEddy <53114885+MaestroEddy@users.noreply.github.com>
2021-01-26 14:15:31 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ import PackageDescription
 
 let package = Package(name: "PSACommon",
                       platforms: [.iOS(.v10)],
-                      products: [.library(name: "PSACommon", targets: ["PSACommon"])],
+                     // products: [.library(name: "PSACommon", targets: ["PSACommon"])],
                       targets: [.target(name: "PSACommon")]
                      )
