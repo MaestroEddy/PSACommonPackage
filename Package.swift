d45aac7f4f50253b125d9d48c1f1e20c7d041d5d
MaestroEddy <53114885+MaestroEddy@users.noreply.github.com>
2021-01-26 12:14:06 +0300

    1.0.2

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 
 import PackageDescription
 
-let package = Package(name: "Alamofire",
+let package = Package(name: "PSACommon",
                       platforms: [ .iOS(.v10) ],
                       products: [.library(name: "PSACommon", targets: ["PSACommon"])],
-                      targets: [.target(name: "PSACommon", .testTarget(name: "AlamofireTests", dependencies: ["Alamofire"], path: "Tests")]
+                      targets: [.target(name: "PSACommon"]
