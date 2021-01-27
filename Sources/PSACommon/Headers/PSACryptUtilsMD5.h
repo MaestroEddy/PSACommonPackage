db2b901ed78fb76d561d394d3584c7a46578842f
MaestroEddy <53114885+MaestroEddy@users.noreply.github.com>
2021-01-27 11:25:21 +0300

    Delete Sources/PSACommon directory

--- Sources/PSACommon/Headers/PSACryptUtilsMD5.h
+++
@@ -1,7 +0,0 @@
-// Copyright © Protectoria. All rights reserved.
-
-#import <Foundation/Foundation.h>
-
-@interface PSACryptUtilsMD5 : NSObject
-+ (nullable NSData *)encode:(nullable NSData *)data;
-@end
