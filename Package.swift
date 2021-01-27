c9d6048302424ca14fe2d725195b5cbbb3c744fa
MaestroEddy <53114885+MaestroEddy@users.noreply.github.com>
2021-01-27 10:09:21 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(name: "PSACommon",
                       platforms: [.iOS(.v10)],
-                      products: [.executable(name: "PSACommon", targets: ["PSACommon"])],
+                      products: [.library(name: "PSACommon", targets: ["PSACommon"])],
                       targets: [.binaryTarget(name: "PSACommon", 
                                               url: "https://github.com/Okaythis/PSACommonIOS/raw/f9ceec9031fce5e90c0cb8b99d2aa8cc58601e37/PSACommonSDK.zip", 
                                               checksum: "6a06e243ae8f9108184b75fbf550bc0154d02ec1")]
