2a9a6b4c172606047d68ba0a3e2179014f419b0d
MaestroEddy <53114885+MaestroEddy@users.noreply.github.com>
2021-01-27 17:12:52 +0300

    Delete PSACommon.xcframework directory

--- PSACommon.xcframework/ios-x86_64-simulator/PSACommon.framework/Headers/PSAAttributedStringUtils.h
+++
@@ -1,8 +0,0 @@
-// Copyright © Protectoria. All rights reserved.
-
-#import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-
-@interface PSAAttributedStringUtils : NSObject
-+ (NSAttributedString *)createFromAttributedString:(NSAttributedString *)attributedString inRange:(NSRange)range withColor:(UIColor *)color;
-@end
--- PSACommon.xcframework/ios-x86_64-simulator/PSACommon.framework/Headers/PSAEllipticButton.h
+++
@@ -1,8 +0,0 @@
-// Copyright © Protectoria. All rights reserved.
-
-#import <UIKit/UIKit.h>
-#import "PSAFlatEllipticButton.h"
-
-@interface PSAEllipticButton : PSAFlatEllipticButton
-
-@end
